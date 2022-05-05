862c663646b2b4befcdd0501cc913587a9ea255e
SusanBird <susanbird391@gmail.com>
2022-05-05 14:22:30 -0700

    built structure of form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>
+            Title
+            <input />
+          </label>
+          <label>
+            Year Released
+            <input />
+          </label>
+          <label>
+            Director
+            <input />
+          </label>
+          <label>
+            Poster Color
+            <input />
+          </label>
+          <button>Add Movie</button>
+        </form>
+        <MoviePoster />
+
+        <MovieList />
       </header>
     </div>
   );
