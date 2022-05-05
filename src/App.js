a448574fac67535e495f59e797d479cecced01a5
SusanBird <susanbird391@gmail.com>
2022-05-05 15:53:57 -0700

    basic css on form

--- src/App.js
+++ src/App.js
@@ -4,28 +4,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form>
-          <label>
+        <div className='top'> 
+          <form>
+            <label>
             Title
-            <input />
-          </label>
-          <label>
+              <input />
+            </label>
+            <label>
             Year Released
-            <input />
-          </label>
-          <label>
+              <input />
+            </label>
+            <label>
             Director
-            <input />
-          </label>
-          <label>
+              <input />
+            </label>
+            <label>
             Poster Color
-            <input />
-          </label>
-          <button>Add Movie</button>
-        </form>
-        <MoviePoster />
-
-        <MovieList />
+              <input />
+            </label>
+            <button>Add Movie</button>
+          </form>
+        </div>
+        {/* <MoviePoster /> */}
+        <div className='bottom'>
+        
+          {/* <MovieList /> */}
+        </div>
       </header>
     </div>
   );
